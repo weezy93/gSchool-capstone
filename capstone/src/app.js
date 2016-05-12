521e4a8b65b4159af1f9bf9dd6a0d0f81d7fc142
Louisa Reese <louisa.e.reese@gmail.com>
2016-05-12 14:22:10 -0600

    hello world!

---
+++ capstone/src/app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Hello Capstone!
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default App;
